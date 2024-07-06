2dc93e8613005f94867b3ef68f8036a4ee885d79
dimass2004 <dimoon019@ya.ru>
2024-07-06 19:18:17 +0300

    Create ex_03.py

---
+++ day_00/ex_03.py
@@ -0,0 +1,15 @@
+import math
+
+def f(x):
+    return math.exp(x)
+
+left = 0
+right = 1
+step = 100
+h = (right - left) / step
+
+s = 0
+for i in range(step):
+    s += h*(f(left+i*h)+ f(left+(i+1)*h))*0.5
+
+print("Результат интегрирования методом левых прямоугольников:", s)
