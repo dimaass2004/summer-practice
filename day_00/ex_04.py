7abc31251c46ee2bab9ae43a68ac5b16e99eb2dc
dimass2004 <dimoon019@ya.ru>
2024-07-08 18:15:24 +0300

    Simpson method

---
+++ day_00/ex_04.py
@@ -0,0 +1,15 @@
+import math
+
+def f(x):
+    return math.exp(x)
+
+left = 0
+right = 1
+step = 10
+h = (right - left) / step
+
+s = 0
+for i in range(step):
+    s += h*(f(left+i*h)+ 4*f(left+h*(i+1/2))+f(left+(i+1)*h))/6
+
+print("Результат интегрирования методом Симпсона:", s)
