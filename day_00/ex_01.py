5d3ba2927fd758b3c07cde7881bb87b7c0373df4
dimass2004 <dimoon019@ya.ru>
2024-07-10 16:24:35 +0300

    добавил графики

--- day_00/ex_01.py
+++ day_00/ex_01.py
@@ -1,20 +1,33 @@
 import math
-
+import matplotlib.pyplot as plt
 def f(x):
     return math.exp(x)
-
 def s(left, right, step):
-    left = 0
-    right = 1
     h = (right - left) / step
     s = 0
     for i in range(step):
-        s += h*f(left + i*h)
-    print("{0:<10}{1:<10}".format(step,abs(s-(math.exp(1)-1))))
-left=0
-right=1
-step=10
-        
-for _ in range (7):
-    s(left, right, step)
+        s += h * f(left + i * h)
+    return s
+left = 0
+right = 1
+step = 10
+steps = []
+errors = []
+for _ in range(7):
+    integral_value = s(left, right, step)
+    error = abs(integral_value - (math.exp(1) - 1))
+    steps.append(step)
+    errors.append(error)
     step *= 10
+print("Step\tError")
+for i in range(len(steps)):
+    print(f"{steps[i]}\t{errors[i]}")
+plt.figure(figsize=(10, 6))
+plt.plot(steps, errors, marker='o')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('Количество шагов')
+plt.ylabel('Ошибка')
+plt.title('Завсисимость ошибки от количесва шагов в методе левых прямоугольников')
+plt.grid(True)
+plt.show()
--- day_00/ex_03.py
+++ day_00/ex_03.py
@@ -1,4 +1,5 @@
 import math
+import matplotlib.pyplot as plt
 
 def f(x):
     return math.exp(x)
@@ -9,11 +10,27 @@ def s(left, right, step):
     s = 0
     for i in range(step):
         s += h*(f(left+i*h)+ f(left+(i+1)*h))*0.5
-    print("{0:<10}{1:<10}".format(step,abs(s-(math.exp(1)-1))))
+    return s
 left=0
 right=1
 step=10
-        
-for _ in range (7):
-    s(left, right, step)
+steps = []
+errors = []
+for _ in range(7):
+    integral_value = s(left, right, step)
+    error = abs(integral_value - (math.exp(1) - 1))
+    steps.append(step)
+    errors.append(error)
     step *= 10
+print("Step\tError")
+for i in range(len(steps)):
+    print(f"{steps[i]}\t{errors[i]}")
+plt.figure(figsize=(10, 6))
+plt.plot(steps, errors, marker='o')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('Количество шагов')
+plt.ylabel('Ошибка')
+plt.title('Завсисимость ошибки от количесва шагов в методе трапеций')
+plt.grid(True)
+plt.show()
